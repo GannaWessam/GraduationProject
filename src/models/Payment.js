063b0c494df75ad1fb82f9105c0bb06d48023832
AhmedAyman71 <ahmedroshdyaa4@gmail.com>
2026-02-17 23:15:56 +0200

    Edit payment table

--- src/models/Payment.js
+++ src/models/Payment.js
@@ -4,8 +4,8 @@ module.exports = (sequelize) => {
   const Payment = sequelize.define('Payment', {
     paymentId: { type: DataTypes.UUID, primaryKey: true, defaultValue: UUIDV4, },
     userId: { type: DataTypes.UUID, allowNull: false,},
-    receiptId: { type: DataTypes.STRING, allowNull: false },
-    orderId: { type: DataTypes.STRING(100), allowNull: false,},
+    receiptId: { type: DataTypes.STRING, allowNull: true },
+    orderId: { type: DataTypes.STRING(100), allowNull: true,},
     timestamp: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
     status: { type: DataTypes.ENUM('PENDING', 'SUCCESS', 'FAILED'), allowNull: false, defaultValue: 'PENDING' },
   }, {

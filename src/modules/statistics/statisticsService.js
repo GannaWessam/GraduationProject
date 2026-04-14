afaf780f8703e969501b41db075c049767555db8
AhmedAyman71 <ahmedroshdyaa4@gmail.com>
2026-04-15 00:12:42 +0200

    Add todayPayment and change payment url

--- src/modules/statistics/statisticsService.js
+++ src/modules/statistics/statisticsService.js
@@ -1,8 +1,33 @@
-const { Op } = require("sequelize");
+const { Op, fn, col } = require("sequelize");
 const { Student } = require("../../models");
-const { course, exam } = require("../../models");
+const { course, exam,Payment } = require("../../models");
 
 // ================= GET STATS =================
+
+async function getTodayTotalPayments() {
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  const endOfDay = new Date();
+  endOfDay.setHours(23, 59, 59, 999);
+
+  const result = await Payment.findOne({
+    attributes: [
+      [fn('SUM', col('actualAmount')), 'total']
+    ],
+    where: {
+      timestamp: {
+        [Op.between]: [startOfDay, endOfDay]
+      },
+      status: 'SUCCESS'
+    },
+    raw: true
+  });
+
+  return result.total || 0;
+}
+
+
 async function getStudentsStats() {
   const successCount = await Student.count({
     where: { status: "succeeded" },
@@ -22,17 +47,19 @@ async function getStudentsStats() {
 
 
   const egyptianCount = await Student.count({
-    where: { nationality: "Egyptian" },
+    where: { nationality: "Egyptian | مصري" },
   });
 
   const otherCount = await Student.count({
     where: {
       [Op.or]: [
-        { nationality: { [Op.ne]: "Egyptian" } },
+        { nationality: { [Op.ne]: "Egyptian | مصري" } },
         { nationality: null },
       ],
     },
   });
+
+  const todayPayments=await getTodayTotalPayments()
 
   return {
     success: successCount,
@@ -40,6 +67,7 @@ async function getStudentsStats() {
     egyptian: egyptianCount,
     other: otherCount,
     Trainees:TraineesCount,
+    todayPayments:todayPayments
   };
 }
 async function getCoursesAndExamsStats() {
